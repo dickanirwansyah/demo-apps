ac618799e024335d7fc0552bb90901aedc8997cd
Muhammad Dicka Nirwansyah <dickanirwansyah@gmail.com>
2024-07-03 22:14:40 +0700

    testing trigger from jenkins

--- src/main/java/com/app/demo_apps_jenkins/DemoAppsJenkinsApplication.java
+++ src/main/java/com/app/demo_apps_jenkins/DemoAppsJenkinsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoAppsJenkinsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("demo-apps-jenkins is starter");
 		SpringApplication.run(DemoAppsJenkinsApplication.class, args);
 	}
 
