7ba41d0c82d215e97cc59e928ea1b33afda6d45b
dickanirwansyah <dickanirwansyah@gmail.com>
2024-07-04 20:31:18 +0700

    add docker file

--- src/main/java/com/app/demo_apps_jenkins/DemoAppsJenkinsApplication.java
+++ src/main/java/com/app/demo_apps_jenkins/DemoAppsJenkinsApplication.java
@@ -3,10 +3,12 @@ package com.app.demo_apps_jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoAppsJenkinsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("demo-apps-jenkins is starter");
 		SpringApplication.run(DemoAppsJenkinsApplication.class, args);
 	}
 
